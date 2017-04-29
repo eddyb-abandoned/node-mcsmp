832c0e4a40eeb2b964936ae2171108937f0638c6
Recursive Lambda God <chris@circle.academy>
2017-04-29 15:33:13 -0400

    "Recursive Lambda Refactor V1"

--- examples/client.js
+++ examples/client.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 
-var mc = require('../'), packets = mc.protocol.packets;
+var mc = require('../');
+var packets = mc.protocol.packets;
 
 function readPassword(cb) {
-    var stdin = process.stdin, stdout = process.stdout, stdio = process.binding('stdio');
+    var stdin = process.stdin;
+    var stdout = process.stdout;
+    var stdio = process.binding('stdio');
     stdio.setRawMode();
-    
+
     stdin.resume();
-    
+
     var password = '';
     function char(c) {
         c = c + '';
@@ -40,18 +43,20 @@ if(process.argv.length < 4) {
     process.exit();
 }
 
-var client = new mc.ClientSocket(), server = process.argv[2], username = process.argv[3];
+var client = new mc.ClientSocket();
+var server = process.argv[2];
+var username = process.argv[3];
 
-client.on('error', function(e) {
+client.on('error', e => {
     //client.emit('close');
     console.log('[ERROR] '+e);
     process.exit();
 });
-client.on('close', function(message) {
+client.on('close', message => {
     console.log('[DISCONNECT] '+message);
     process.exit();
 });
-client.on('packet', function(packet) {
+client.on('packet', packet => {
     if(packet.type == packets.LOGIN) {
         setTimeout(client.write.bind(client, packets.CHAT, {message: 'Hi, this is a demo client of node-mcsmp. Please ignore me :).'}), 2000);
     } else if(packet.type == packets.CHAT)
@@ -59,7 +64,7 @@ client.on('packet', function(packet) {
 });
 
 console.log('Please enter your password (leave blank for no authentication):');
-readPassword(function(password) {
+readPassword(password => {
     if(!password)
         password = undefined;
     client.connect(server, username, password);
